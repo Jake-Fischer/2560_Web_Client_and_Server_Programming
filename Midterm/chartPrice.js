1184d82113f51d5ec3acc172ca695a11f676fc63
Jake-Fischer <Taylormade123!>
2021-03-17 21:35:00 -0500

    Added more comments and improved structure of the page and code

--- Midterm/chartPrice.js
+++ Midterm/chartPrice.js
@@ -1,22 +1,26 @@
 function chartPrice(rates,times) {
+    //Log for testing
+    //console.log(rates)
+    //Get context
     let ctx = document.querySelector('#price-chart');
+    //Create the chart
     let lineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: times,
+            labels: times, //Times array is passed to this function
             datasets: [
                 {
                     label:'Price',
-                    data:rates
-                }
-            ]
+                    data: rates //Rates array is passed to this function
+                }               //Something is up with this, my x axis is filling with times just as it should
+            ]                   //But there are no points on my grid I have yet to figure this out
         },
         options: {
             scales:{
                 yAxes:[{
                     ticks:{
-                        beginAtZero: true,
-                        suggestedMax: 1000000,
+                        beginAtZero: true, //Default grid structure, this must be changed
+                        suggestedMax: 100000,
                         suggestedMin: 1000,
                         stepSize: 1,
                         maxTicksLimit:10
--- Midterm/main.js
+++ Midterm/main.js
@@ -1,60 +1,67 @@
-//Test to see main works
-let test = document.querySelector('#test')
-test.innerHTML = 'Working'
-
-let update = 10000
-let maxFailedAttempts = 3 
 
 let currencySelection = document.querySelector('#currency-select')
+
+//Default data to start USD chart with 
 let defaultCurrency = 'USD'
+let chartData = [] //Will hold objects containing price and time variables
+let rates = [] //Will hold rates
+let times = [] //Will hold times
 
-let chartData = []
-
-let rates = []
-
-let times = []
-
+//Call main function to start
 getCurrentXPI(defaultCurrency, chartData, rates, times)
 
 function getCurrentXPI(currency, chartData, rates, times){
+    //URL with USD as currency to start chart 1
     let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json` //URL string, with country code
     fetch(url).then(res => { //Fetch Response
         return res.json() //Put it in a json format
     }).then((data) =>{ //Take that data, and do the following with it
         
         console.log(data)
-        
+        //Show the price and time received
         let bpiParagraph = document.querySelector('#BPI')
         let timeParagraph = document.querySelector('#time-retreived')
-
         bpiParagraph.innerHTML = data.bpi.USD.rate
         timeParagraph.innerHTML = data.time.updated
 
+        //Save price and time, and create timeRate object
         let currentRate = data.bpi.USD.rate
         let currentTime = data.time.updated
-
         let timeRate = {'x': currentTime, 'y': currentRate}
 
         setTimeout( function() {
 
+            //log for testing
             console.log(timeRate)
 
+            //Add timeRate to chartData
             chartData.push(timeRate)
-
+            //Add rate to rates array
             rates.push(currentRate)
+            //Add times to times array
             times.push(currentTime)
 
+            //log for testing
             console.log(chartData)
             console.log(rates)
             console.log(times)
 
+            //Call chart price with the times and the rates, this will redraw the chart with the current numbers
             chartPrice(rates,times)
 
-            getCurrentXPI(currency,chartData, rates, times)
+            //Call main again to restart
+            getCurrentXPI(currency, chartData, rates, times)
 
-        }, 5000)
+        }, 60000) //Wait 60 seconds after doing this, to ensure API has responded. This api also only updates the price server side every 1 minute
     }).catch((error) =>{ //Error handling
         console.log(error)
         alert('An error has occured! Please try again.')
     })
 }
+
+//To do
+//1. Add a second chart bellow that begins when one enters in a valid currency
+// this chart will update every minute aswell, if a new currency is entered
+// this chart will switch to tracking the new currency
+//2. When the program first starts, get the last ten minutes of activity and plot it first
+//3. Then run the main program, which will remove the last piece of data from the array and add the current price
