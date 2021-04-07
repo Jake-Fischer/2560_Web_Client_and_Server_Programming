ab4ed84731c55b8fd4b5c42d61edf013831d03b1
Jake-Fischer <Taylormade123!>
2021-04-06 20:25:18 -0500

    videos for week 10 started

---
+++ web_week_10/class10/would_you_rather/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>Would you Rather...</h1>
+
+    <would-you-rather 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"
+    ></would-you-rather>
+    <p>{{userSelectionMessage}}</p>
+  </div>
+</template>
+
+<script>
+import WouldYouRather from './components/WouldYouRather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather become five years older or two years younger?',
+      wyrAnswer1: 'Five Years Older',
+      wyrAnswer2: 'Two Years Younger',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks, you chose: ${choice}`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
